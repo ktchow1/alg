54afef107a32162fea1b3cf46b8ad123de8b6cc9
ktchow1 <ktchow1@yahoo.com.hk>
2025-06-12 01:00:13 +0100

    alignment

--- alg/04_fundalmental/test/test_memory_alignment.cpp
+++ alg/04_fundalmental/test/test_memory_alignment.cpp
@@ -22,18 +22,26 @@
 // * alignof(struct {char,char,std::uint32_t }) = 4
 //
 //
-// ******************** //
-// *** Zero padding *** //
-// ******************** //
-// Zeros are padded in order to fulfill alignof(POD). The algorithm of zero padding is like : 
 //
+// ****************************************** //
+// *** Address of member and zero padding *** //
+// ****************************************** //
+// Address of each member in POD is :
 //
+//    addr_of(mem[n]) = (min wrt k) max[ addr_of(mem[n-1]) + sizeof(mem[n-1]), k * alignof(mem[n]) ]
+//
+// then zeros are padded to the end of mem[n-1] in this range : 
+//
+//    addr_of(mem[n-1]) + sizeof(mem[n-1]) <---> addr_of(mem[n])
 //
 //
 //
 // ******************* //
 // *** Size of POD *** //
 // ******************* //
+// Size of POD is :
+//
+//    sizeof(POD) = (min wrt k) max[ addr_of(mem[n]) + sizeof(mem[n]), k * alignof(POD) ]
 //
 //
 
@@ -77,83 +85,83 @@ void check_quad_class(std::uint32_t align, std::uint32_t size, std::uint32_t m0,
     assert((std::uint64_t)(&x.m3) - (std::uint64_t)(&x) == m3);
 }
 
-using A01     =   bi_class<std:: uint8_t, std::uint16_t>;
-using A10     =   bi_class<std::uint16_t, std:: uint8_t>;
+using A12     =   bi_class<std:: uint8_t, std::uint16_t>;
+using A21     =   bi_class<std::uint16_t, std:: uint8_t>;
 
-using B012    =  tri_class<std:: uint8_t, std::uint16_t, std::uint32_t>;
-using B021    =  tri_class<std:: uint8_t, std::uint32_t, std::uint16_t>;
-using B102    =  tri_class<std::uint16_t, std:: uint8_t, std::uint32_t>;
-using B120    =  tri_class<std::uint16_t, std::uint32_t, std:: uint8_t>;
-using B201    =  tri_class<std::uint32_t, std:: uint8_t, std::uint16_t>;
-using B210    =  tri_class<std::uint32_t, std::uint16_t, std:: uint8_t>;
+using B124    =  tri_class<std:: uint8_t, std::uint16_t, std::uint32_t>;
+using B142    =  tri_class<std:: uint8_t, std::uint32_t, std::uint16_t>;
+using B214    =  tri_class<std::uint16_t, std:: uint8_t, std::uint32_t>;
+using B241    =  tri_class<std::uint16_t, std::uint32_t, std:: uint8_t>;
+using B412    =  tri_class<std::uint32_t, std:: uint8_t, std::uint16_t>;
+using B421    =  tri_class<std::uint32_t, std::uint16_t, std:: uint8_t>;
 
-using C0123   = quad_class<std:: uint8_t, std::uint16_t, std::uint32_t, std::uint64_t>;
-using C0132   = quad_class<std:: uint8_t, std::uint16_t, std::uint64_t, std::uint32_t>;
-using C0213   = quad_class<std:: uint8_t, std::uint32_t, std::uint16_t, std::uint64_t>;
-using C0231   = quad_class<std:: uint8_t, std::uint32_t, std::uint64_t, std::uint16_t>;
-using C0312   = quad_class<std:: uint8_t, std::uint64_t, std::uint16_t, std::uint32_t>;
-using C0321   = quad_class<std:: uint8_t, std::uint64_t, std::uint32_t, std::uint16_t>;
+using C1248   = quad_class<std:: uint8_t, std::uint16_t, std::uint32_t, std::uint64_t>;
+using C1284   = quad_class<std:: uint8_t, std::uint16_t, std::uint64_t, std::uint32_t>;
+using C1428   = quad_class<std:: uint8_t, std::uint32_t, std::uint16_t, std::uint64_t>;
+using C1482   = quad_class<std:: uint8_t, std::uint32_t, std::uint64_t, std::uint16_t>;
+using C1824   = quad_class<std:: uint8_t, std::uint64_t, std::uint16_t, std::uint32_t>;
+using C1842   = quad_class<std:: uint8_t, std::uint64_t, std::uint32_t, std::uint16_t>;
 
-using C1023   = quad_class<std::uint16_t, std:: uint8_t, std::uint32_t, std::uint64_t>;
-using C1032   = quad_class<std::uint16_t, std:: uint8_t, std::uint64_t, std::uint32_t>;
-using C1203   = quad_class<std::uint16_t, std::uint32_t, std:: uint8_t, std::uint64_t>;
-using C1230   = quad_class<std::uint16_t, std::uint32_t, std::uint64_t, std:: uint8_t>;
-using C1302   = quad_class<std::uint16_t, std::uint64_t, std:: uint8_t, std::uint32_t>;
-using C1320   = quad_class<std::uint16_t, std::uint64_t, std::uint32_t, std:: uint8_t>;
+using C2148   = quad_class<std::uint16_t, std:: uint8_t, std::uint32_t, std::uint64_t>;
+using C2184   = quad_class<std::uint16_t, std:: uint8_t, std::uint64_t, std::uint32_t>;
+using C2418   = quad_class<std::uint16_t, std::uint32_t, std:: uint8_t, std::uint64_t>;
+using C2481   = quad_class<std::uint16_t, std::uint32_t, std::uint64_t, std:: uint8_t>;
+using C2814   = quad_class<std::uint16_t, std::uint64_t, std:: uint8_t, std::uint32_t>;
+using C2841   = quad_class<std::uint16_t, std::uint64_t, std::uint32_t, std:: uint8_t>;
 
-using C2013   = quad_class<std::uint32_t, std:: uint8_t, std::uint16_t, std::uint64_t>;
-using C2031   = quad_class<std::uint32_t, std:: uint8_t, std::uint64_t, std::uint16_t>;
-using C2103   = quad_class<std::uint32_t, std::uint16_t, std:: uint8_t, std::uint64_t>;
-using C2130   = quad_class<std::uint32_t, std::uint16_t, std::uint64_t, std:: uint8_t>;
-using C2301   = quad_class<std::uint32_t, std::uint64_t, std:: uint8_t, std::uint16_t>;
-using C2310   = quad_class<std::uint32_t, std::uint64_t, std::uint16_t, std:: uint8_t>;
+using C4128   = quad_class<std::uint32_t, std:: uint8_t, std::uint16_t, std::uint64_t>;
+using C4182   = quad_class<std::uint32_t, std:: uint8_t, std::uint64_t, std::uint16_t>;
+using C4218   = quad_class<std::uint32_t, std::uint16_t, std:: uint8_t, std::uint64_t>;
+using C4281   = quad_class<std::uint32_t, std::uint16_t, std::uint64_t, std:: uint8_t>;
+using C4812   = quad_class<std::uint32_t, std::uint64_t, std:: uint8_t, std::uint16_t>;
+using C4821   = quad_class<std::uint32_t, std::uint64_t, std::uint16_t, std:: uint8_t>;
 
-using C3012   = quad_class<std::uint64_t, std:: uint8_t, std::uint16_t, std::uint32_t>;
-using C3021   = quad_class<std::uint64_t, std:: uint8_t, std::uint32_t, std::uint16_t>;
-using C3102   = quad_class<std::uint64_t, std::uint16_t, std:: uint8_t, std::uint32_t>;
-using C3120   = quad_class<std::uint64_t, std::uint16_t, std::uint32_t, std:: uint8_t>;
-using C3201   = quad_class<std::uint64_t, std::uint32_t, std:: uint8_t, std::uint16_t>;
-using C3210   = quad_class<std::uint64_t, std::uint32_t, std::uint16_t,  std::uint8_t>;
+using C8124   = quad_class<std::uint64_t, std:: uint8_t, std::uint16_t, std::uint32_t>;
+using C8142   = quad_class<std::uint64_t, std:: uint8_t, std::uint32_t, std::uint16_t>;
+using C8214   = quad_class<std::uint64_t, std::uint16_t, std:: uint8_t, std::uint32_t>;
+using C8241   = quad_class<std::uint64_t, std::uint16_t, std::uint32_t, std:: uint8_t>;
+using C8412   = quad_class<std::uint64_t, std::uint32_t, std:: uint8_t, std::uint16_t>;
+using C8421   = quad_class<std::uint64_t, std::uint32_t, std::uint16_t,  std::uint8_t>;
 
 void test_memory_alignment()
 {
-    check_bi_class<A01>(2, 4, 0, 2);
-    check_bi_class<A10>(2, 4, 0, 2);
+    check_bi_class<A12>(2, 4, 0, 2);
+    check_bi_class<A21>(2, 4, 0, 2);
 
-    check_tri_class<B012>(4,  8, 0, 2, 4);
-    check_tri_class<B021>(4, 12, 0, 4, 8);
-    check_tri_class<B102>(4,  8, 0, 2, 4);
-    check_tri_class<B120>(4, 12, 0, 4, 8);
-    check_tri_class<B201>(4,  8, 0, 4, 6);
-    check_tri_class<B210>(4,  8, 0, 4, 6);
+    check_tri_class<B124>(4,  8, 0, 2, 4);
+    check_tri_class<B142>(4, 12, 0, 4, 8);
+    check_tri_class<B214>(4,  8, 0, 2, 4);
+    check_tri_class<B241>(4, 12, 0, 4, 8);
+    check_tri_class<B412>(4,  8, 0, 4, 6);
+    check_tri_class<B421>(4,  8, 0, 4, 6);
 
-    check_quad_class<C0123>(8, 16, 0, 2,  4,  8);
-    check_quad_class<C0132>(8, 24, 0, 2,  8, 16);
-    check_quad_class<C0213>(8, 24, 0, 4,  8, 16);
-    check_quad_class<C0231>(8, 24, 0, 4,  8, 16);
-    check_quad_class<C0312>(8, 24, 0, 8, 16, 20);
-    check_quad_class<C0321>(8, 24, 0, 8, 16, 20);
+    check_quad_class<C1248>(8, 16, 0, 2,  4,  8);
+    check_quad_class<C1284>(8, 24, 0, 2,  8, 16);
+    check_quad_class<C1428>(8, 24, 0, 4,  8, 16);
+    check_quad_class<C1482>(8, 24, 0, 4,  8, 16);
+    check_quad_class<C1824>(8, 24, 0, 8, 16, 20);
+    check_quad_class<C1842>(8, 24, 0, 8, 16, 20);
                          
-    check_quad_class<C1023>(8, 16, 0, 2,  4,  8);
-    check_quad_class<C1032>(8, 24, 0, 2,  8, 16);
-    check_quad_class<C1203>(8, 24, 0, 4,  8, 16);
-    check_quad_class<C1230>(8, 24, 0, 4,  8, 16);
-    check_quad_class<C1302>(8, 24, 0, 8, 16, 20);
-    check_quad_class<C1320>(8, 24, 0, 8, 16, 20);
+    check_quad_class<C2148>(8, 16, 0, 2,  4,  8);
+    check_quad_class<C2184>(8, 24, 0, 2,  8, 16);
+    check_quad_class<C2418>(8, 24, 0, 4,  8, 16);
+    check_quad_class<C2481>(8, 24, 0, 4,  8, 16);
+    check_quad_class<C2814>(8, 24, 0, 8, 16, 20);
+    check_quad_class<C2841>(8, 24, 0, 8, 16, 20);
                          
-    check_quad_class<C2013>(8, 16, 0, 4,  6,  8);
-    check_quad_class<C2031>(8, 24, 0, 4,  8, 16);
-    check_quad_class<C2103>(8, 16, 0, 4,  6,  8);
-    check_quad_class<C2130>(8, 24, 0, 4,  8, 16);
-    check_quad_class<C2301>(8, 24, 0, 8, 16, 18);
-    check_quad_class<C2310>(8, 24, 0, 8, 16, 18);
+    check_quad_class<C4128>(8, 16, 0, 4,  6,  8);
+    check_quad_class<C4182>(8, 24, 0, 4,  8, 16);
+    check_quad_class<C4218>(8, 16, 0, 4,  6,  8);
+    check_quad_class<C4281>(8, 24, 0, 4,  8, 16);
+    check_quad_class<C4812>(8, 24, 0, 8, 16, 18);
+    check_quad_class<C4821>(8, 24, 0, 8, 16, 18);
                          
-    check_quad_class<C3012>(8, 16, 0, 8, 10, 12);
-    check_quad_class<C3021>(8, 24, 0, 8, 12, 16);
-    check_quad_class<C3102>(8, 16, 0, 8, 10, 12);
-    check_quad_class<C3120>(8, 24, 0, 8, 12, 16);
-    check_quad_class<C3201>(8, 16, 0, 8, 12, 14);
-    check_quad_class<C3210>(8, 16, 0, 8, 12, 14);
+    check_quad_class<C8124>(8, 16, 0, 8, 10, 12);
+    check_quad_class<C8142>(8, 24, 0, 8, 12, 16);
+    check_quad_class<C8214>(8, 16, 0, 8, 10, 12);
+    check_quad_class<C8241>(8, 24, 0, 8, 12, 16);
+    check_quad_class<C8412>(8, 16, 0, 8, 12, 14);
+    check_quad_class<C8421>(8, 16, 0, 8, 12, 14);
 
     print_summary("memory alignment for POD", "succeeded");
 }
