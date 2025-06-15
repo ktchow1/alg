6e95dbd196d557a607ac483fda57a1d8fab9e21a
ktchow1 <ktchow1@yahoo.com.hk>
2025-06-15 18:10:18 +0100

    update option

---
+++ alg/05_template/include/function_bind.h
@@ -0,0 +1,35 @@
+#pragma once
+#include<functional>
+
+
+// ************************************************************ //
+// This alg::bind works with std::function, NOT alg::function.
+// 
+//
+//
+//
+// ************************************************************ //
+
+namespace alg
+{
+    template<typename...ARGS>
+    class bind_function
+    {
+    public:
+
+
+
+        // ****************** //
+        // *** Invocation *** //
+        // ****************** //
+
+
+
+    private:
+
+    };
+
+    
+
+
+}
