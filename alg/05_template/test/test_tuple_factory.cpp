ba5b2a1756abdcb7910824942c5103c763055715
ktchow1 <ktchow1@yahoo.com.hk>
2025-06-22 22:52:17 +0100

    update valueness

--- alg/05_template/test/test_tuple_factory.cpp
+++ alg/05_template/test/test_tuple_factory.cpp
@@ -167,6 +167,7 @@ void test_make_tuple()
     char c,c0,c1;
     int  i,i0,i1;
    
+
     // case 1 : make_tuple<T>
     b = true, c = 'a', i = 123;
     auto tup0 = alg::make_tuple(b,c,i);
@@ -179,6 +180,7 @@ void test_make_tuple()
     assert(std::get<1>(tup0) == 'a');
     assert(std::get<2>(tup0) == 123); // modify failed
 
+
     // case 2 : make_tuple<std::reference_wrapper<T>> 
     b = true, c = 'a', i = 123;
     auto tup1 = alg::make_tuple(std::ref(b), std::ref(c), std::ref(i)); // Remark : We can make the assignment
@@ -190,6 +192,7 @@ void test_make_tuple()
     assert(std::get<0>(tup1).get()== false); 
     assert(std::get<1>(tup1).get()== 'b');
     assert(std::get<2>(tup1).get()== 234); // modify succeed
+
 
     // case 3 : tie to tuple<T>
     alg::tie(b0,c0,i0) = tup0;
@@ -204,6 +207,7 @@ void test_make_tuple()
     assert(c0 == 'a');
     assert(i0 == 123); // Remark : Tied variables is a temporary tuple, it cannot track source tuple<T> change.
 
+
     // case 4 : tie to tuple<std::reference_wrapper<T>>
     alg::tie(b1,c1,i1) = tup1;
     assert(b1 == false);
@@ -217,6 +221,7 @@ void test_make_tuple()
     assert(c1 == 'b');
     assert(i1 == 234); // Remark : Tied variables is a temporary tuple, it cannot track source tuple<T&> change.
 
+
     // case 5 : structural binding of variables to tuple<T>
     auto [b2,c2,i2] = tup0;
     assert(b2 == false);
@@ -229,6 +234,7 @@ void test_make_tuple()
     assert(b2 == false);
     assert(c2 == 'b');
     assert(i2 == 234); // modify failed
+
 
     // case 6 : structural binding of references to tuple<T>
     auto& [b3,c3,i3] = tup0;
@@ -247,24 +253,9 @@ void test_make_tuple()
 }
 
 
-void test_tie()
-{
-    print_summary("tuple factory - alg::tie", "succeeded");
-}
-
-
-void test_forward_as_tuple()
-{
-    print_summary("tuple factory - alg::forward_as_tuple", "succeeded");
-}
-
-
 void test_tuple_factory()
 {
     test_std_make_tuple_type();
     test_alg_make_tuple_type();
-
     test_make_tuple();
-    test_tie();
-    test_forward_as_tuple();
 }
